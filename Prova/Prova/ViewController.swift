da585b61fa42ad4062463be5aab949e7f3ebbfab
FrancoLuigi <39093575+FrancoLuigi@users.noreply.github.com>
2018-10-09 19:27:50 +0200

    Database

---
+++ Prova/Prova/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prova
+//
+//  Created by francoluigi on 03/10/18.
+//  Copyright © 2018 francoluigi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        
+        
+       // button.layer.cornerRadius = button.layer.frame.size.width/2;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
